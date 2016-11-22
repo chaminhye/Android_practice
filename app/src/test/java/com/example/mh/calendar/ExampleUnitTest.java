f670d20d7586bbfee8339b4b7647af805e448cc6
cha_93 <minhye_love@naver.com>
2016-11-22 17:09:55 +0900

    splash

--- app/src/test/java/com/example/mh/calendar/ExampleUnitTest.java
+++ app/src/test/java/com/example/mh/calendar/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package com.example.mh.calendar;
 
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,9 +12,12 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ExampleUnitTest extends ApplicationTestCase<Application>{
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+    public ExampleUnitTest() {
+        super(Application.class);
     }
 }
